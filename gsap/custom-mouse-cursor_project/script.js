42059b1247c3e2daee55845869cd7138f2c3a5e8
Dipesh Kumar <dipeshkumar0853822@gmail.com>
2026-01-21 15:37:25 +0530

    Built First prototype

--- gsap/custom-mouse-cursor_project/script.js
+++ gsap/custom-mouse-cursor_project/script.js
@@ -1,7 +1,9 @@
-var main = document.querySelectora("#main")
+var main = document.querySelector("#main")
 var cursor = document.querySelector("#cursor")
 
 main.addEventListener("mousemove", function(dets){
-    cursor.style.left = dets.x+"px"
-    cursor.style.top = dets.y+"px"
+    gsap.to(cursor,{
+        x:dets.x,
+        y:dets.y,
+    })
 })
